f8653ab6235e8fadde8b9c9f43295895c20e0523
nquinbla <nquinbla@myuax.com>
2024-04-23 20:03:46 +0200

    [Módulo A] código pt1

---
+++ src/main/java/A_Tiempo/Tiempo.java
@@ -0,0 +1,31 @@
+package A_Tiempo;
+
+public class Tiempo {
+    public static void main(String[] args) {
+        short daysInYear = 365;
+        short hoursInDay = 24;
+        short minutesInHour = 60;
+        short secondsInMinute = 60;
+
+        int years = 1;
+        int months = 2;
+        int days = 3;
+
+        long totalSeconds = calculateSeconds(years, months, days, daysInYear, hoursInDay, minutesInHour, secondsInMinute);
+        System.out.println("Total seconds: " + totalSeconds);
+
+        long totalDays = calculateDays(years, months, days, daysInYear);
+        System.out.println("Total days: " + totalDays);
+    }
+
+    public static long calculateSeconds(int years, int months, int days, short daysInYear, short hoursInDay, short minutesInHour, short secondsInMinute) {
+        int totalDays = years * daysInYear + months * 30 + days;
+        long totalHours = totalDays * hoursInDay;
+        long totalMinutes = totalHours * minutesInHour;
+        return totalMinutes * secondsInMinute;
+    }
+
+    public static long calculateDays(int years, int months, int days, short daysInYear) {
+        return years * daysInYear + months * 30 + days;
+    }
+}
